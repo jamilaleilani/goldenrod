3ad53b944a1476cad90a87823322fde93b031196
keba <keba@adobe.com>
2021-10-03 22:20:00 -0700

    save

---
+++ src/landing.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function Landing() {
+  return (
+    <div className="landing">
+      <div className="the">the</div>
+      <div className="conference">conference</div>
+      <div className="twenty">20</div>
+      <div className="twentyone">21</div>
+    </div>
+  );
+}
+
+export default Landing;
---
+++ src/schedule.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+function Schedule() {
+  return (
+    <div className="schedule">
+    </div>
+  );
+}
+
+export default Schedule;
